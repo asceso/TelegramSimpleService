255ff1ac083f3bfd79215232fe3892455f258e90
Uchidero Krauch <asceso111@gmail.com>
2022-01-11 18:01:39 +0300

    Добавьте файлы проекта.

---
+++ TelegramSimpleBot/ITelegramService.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramSimpleBot
+{
+    public interface ITelegramService
+    {
+        /// <summary>
+        /// Check exist TG token
+        /// </summary>
+        /// <param name="token">token</param>
+        /// <returns>true if token not broken</returns>
+        Task<bool> CheckBotToken(string token);
+        /// <summary>
+        /// Create main bot
+        /// </summary>
+        /// <param name="token">token</param>
+        /// <returns>TelegramBotClient</returns>
+        Task<TelegramBotClient> CreateMainBot(string token);
+        /// <summary>
+        /// Create debugger bot
+        /// </summary>
+        /// <param name="token">token</param>
+        /// <returns>TelegramBotClient</returns>
+        Task<TelegramBotClient> CreateDebugBot(string token);
+        /// <summary>
+        /// Start receiving for main bot
+        /// </summary>
+        /// <param name="updateHandler">handler for updates</param>
+        /// <param name="allowedTypes">allowed update types</param>
+        /// <returns></returns>
+        Task<CancellationTokenSource> StartMainBotReceiving(IUpdateHandler updateHandler, params UpdateType[] allowedTypes);
+        /// <summary>
+        /// Send debug message from debugger bot
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message</param>
+        /// <returns></returns>
+        Task SendLog(long uid, string message);
+        /// <summary>
+        /// Send clear keyboard message
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message</param>
+        /// <returns></returns>
+        Task SendRemoveMessage(long uid, string message);
+        /// <summary>
+        /// Send simple text message
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message</param>
+        /// <returns></returns>
+        Task SendMessage(long uid, string message);
+        /// <summary>
+        /// Send message with file
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message (caption)</param>
+        /// <param name="fs">file stream of file FileMode.Open</param>
+        /// <param name="fileName">filename in telegram</param>
+        /// <param name="deleteFileWhenComplete">delete file when send complete</param>
+        /// <returns></returns>
+        Task SendMessageWithFile(long uid, string message, FileStream fs, string fileName, bool deleteFileWhenComplete = true);
+        /// <summary>
+        /// Send message with keyboard reply
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message</param>
+        /// <param name="markup">ReplyKeyboardMarkup</param>
+        /// <returns></returns>
+        Task SendMessageWithKeyboard(long uid, string message, ReplyKeyboardMarkup markup);
+        /// <summary>
+        /// Send message with inline buttons
+        /// </summary>
+        /// <param name="uid">uid user</param>
+        /// <param name="message">message</param>
+        /// <param name="markup">InlineKeyboardMarkup</param>
+        /// <returns></returns>
+        Task SendMessageWithKeyboard(long uid, string message, InlineKeyboardMarkup markup);
+    }
+}
---
+++ TelegramSimpleBot/TelegramService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramSimpleBot
+{
+    public class TelegramService : ITelegramService
+    {
+        private static TelegramBotClient Client;
+        private static TelegramBotClient Debugger;
+
+        public async Task<bool> CheckBotToken(string token)
+        {
+            try
+            {
+                var client = new TelegramBotClient(token);
+                var bot = await Client.GetMeAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public Task<TelegramBotClient> CreateMainBot(string token)
+        {
+            try
+            {
+                Client = new TelegramBotClient(token);
+                return Task.FromResult(Client);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public Task<TelegramBotClient> CreateDebugBot(string token)
+        {
+            try
+            {
+                Debugger = new TelegramBotClient(token);
+                return Task.FromResult(Debugger);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public Task<CancellationTokenSource> StartMainBotReceiving(IUpdateHandler updateHandler, params UpdateType[] allowedTypes)
+        {
+            if (Client != null)
+            {
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Client.StartReceiving(
+                    updateHandler,
+                    receiverOptions: new ReceiverOptions()
+                    {
+                        AllowedUpdates = allowedTypes
+                    },
+                    cancellationToken: cts.Token
+                    );
+                return Task.FromResult(cts);
+            }
+            return null;
+        }
+        public async Task SendLog(long uid, string message)
+        {
+            try
+            {
+                await Debugger.SendTextMessageAsync(uid, message);
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public async Task SendRemoveMessage(long uid, string message) => await Client.SendTextMessageAsync(uid, message, replyMarkup: new ReplyKeyboardRemove());
+        public async Task SendMessage(long uid, string message) => await Client.SendTextMessageAsync(uid, message, ParseMode.Html);
+        public async Task SendMessageWithFile(long uid, string message, FileStream fs, string fileName, bool deleteFileWhenComplete = true)
+        {
+            await Client.SendDocumentAsync(uid, new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs, fileName), caption: message);
+            fs.Close();
+            if (deleteFileWhenComplete)
+            {
+                System.IO.File.Delete(fs.Name);
+            }
+        }
+        public async Task SendMessageWithKeyboard(long uid, string message, ReplyKeyboardMarkup markup) => await Client.SendTextMessageAsync(uid, message, ParseMode.Html, replyMarkup: markup);
+        public async Task SendMessageWithKeyboard(long uid, string message, InlineKeyboardMarkup markup) => await Client.SendTextMessageAsync(uid, message, ParseMode.Html, replyMarkup: markup);
+    }
+}
